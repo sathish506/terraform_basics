201865e99c551ce463039596a74f327a4b5a92bc
Sathish <sathishu269@gmail.com>
2023-09-19 08:07:46 +0530

    Auto Push

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -42,5 +42,9 @@ variable "sample_list" {
     ]
 }
 
-        
+   output "Print_List" {
+    value = "Welcome to ${var.sample_list[0]} Training and the batch number is ${var.sample_list[1]} and the class number is ${var.sample_list[3]} "
+   }
+
+
 
